2859e12338de3c6661b426503be757531b7a3f21
RelexSun <relexsunnop@gmail.com>
2023-11-21 14:53:17 +0700

    alg.5.7

---
+++ PythonAlgorithm/Alg.5.5/solve.py
@@ -0,0 +1,17 @@
+def binsearch(low, high, n, s) :
+    if low == high :
+        return -1 if s[low] % 2 == 1 else low
+    else :
+        mid = (low + high) // 2
+        if s[mid] % 2 == 0 :
+            return binsearch(low, mid, n, s)
+        else :
+            return binsearch(mid + 1, high, n, s)
+
+def solve(n, s) :
+    j = binsearch(0, n - 1, n, s)
+    print(0 if j < 0 else s[j])
+    
+N = int(input())
+S = list(map(int, input().split()))
+solve(N, S)
