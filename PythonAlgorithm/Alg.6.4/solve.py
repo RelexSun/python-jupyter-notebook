1ad0693cbdfec9ce5ff5b9112fcd2b7ca9c41d00
RelexSun <relexsunnop@gmail.com>
2023-11-23 11:51:37 +0700

    alg.6.5

---
+++ PythonAlgorithm/Alg.6.4/solve.py
@@ -0,0 +1,17 @@
+def solve(x) :
+    stack = []
+    for x in X :
+        if x.isdigit() :
+            stack.append(int(x))
+        else :
+            b, a = stack.pop(), stack.pop()
+            if x == '+' : stack.append(a+b) 
+            elif x == '-' : stack.append(a-b)
+            elif x == '*' : stack.append(a*b)
+            elif x == '/' : stack.append(a//b)
+        print(stack)
+    return stack[-1]
+
+X = input()
+print(X)
+print(solve(X))        
